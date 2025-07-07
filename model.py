bea864a2c32d7a86ddeb1e67360504a541a63eff
AdityaJogdand <160385303+AdityaJogdand@users.noreply.github.com>
2025-07-07 20:19:30 +0530

    Embedding + Position Encoding

--- model.py
+++ model.py
@@ -24,3 +24,13 @@ class PositionalEncoding(nn.Module):
         pe = torch.zero(seq_len, d_model)
         position = torch.arange(0 , seq_len, dtype=torch.flaot).unsqueeze(1) #shape(seq_len , 1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)  
+        pe[:, 1::2] = torch.cos(position * div_term)  
+        
+        pe = pe.unsqueeze(0) #(1 , seq_lens, d_model)
+        
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
