33be36418aa1ca52222496b47acac9d630fa25b3
AdityaJogdand <160385303+AdityaJogdand@users.noreply.github.com>
2025-07-08 22:34:38 +0530

    Layer Normalization and Feed Forward

--- model.py
+++ model.py
@@ -33,4 +33,27 @@ class PositionalEncoding(nn.Module):
 
     def forward(self, x):
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
-        return self.dropout(x)
+        return self.dropout(x) 
+    
+class  LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 10**-6):
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) #multiplied
+        self.bias = nn.Parameter(torch.zeros(1)) # added
+
+    def forward(self, x ):
+        mean = x.mean(dim = -1, keepdim = True)
+        std = x.std(dim = -1, keepdim = True)
+        return self.alpha * (x - mean) / (std + self.eps)  + self.bias
+ 
+class FeedForward(nn.Module):
+    def __init__(self, d_model: int , d_ff: int, dropout: float):
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff , d_model)
+
+    def forward(self, x):
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+    
